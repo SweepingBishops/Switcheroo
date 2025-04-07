cc18221a23d339d47ec3adc2ddc78bbd624eea0c
Harisankar <94229107+Methaphur@users.noreply.github.com>
2025-04-07 21:43:46 +0530

    passwords generated are written to users.json

--- generate_passords.py
+++ generate_passords.py
@@ -1,15 +1,20 @@
 import hashlib
+import json
+
 
 def generate_hashed_password(password):
     # Encode the password to bytes, then hash it using SHA-256
     hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
     return hashed_password
 
+
 # Dictionary containing team names and passwords
 teams = {
     "team1": "password1",
     "team2": "password2",
-    "team3": "password3"
+    "team3": "password3",
+    "testing": "test123",
+    "admin": "admin123",
 }
 
 hashed_passwords = {}
@@ -21,3 +26,10 @@ for team, password in teams.items():
 
 for team, hashed_password in hashed_passwords.items():
     print(f"{team}: {hashed_password}")
+
+# Write the hashed passwords to a JSON file
+output_file = "static/users.json"
+with open(output_file, "w") as file:
+    json.dump(hashed_passwords, file, indent=4, sort_keys=True)
+
+print(f"Hashed passwords have been written to {output_file}")
